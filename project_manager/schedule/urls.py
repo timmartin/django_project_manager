c652c6b5d40aee8544780311aab47d67e717d9a0
Tim Martin <tim@asymptotic.co.uk>
2016-02-13 08:18:13 +0000

    Tasks can be edited

--- project_manager/schedule/urls.py
+++ project_manager/schedule/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'schedule'
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^task/(?P<pk>[0-9]+)/edit/$', views.edit_task, name='edit_task')
 ]
--- project_manager/schedule/views.py
+++ project_manager/schedule/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Task
 
@@ -7,3 +7,16 @@ def index(request):
 
     context = {'tasks': tasks}
     return render(request, 'schedule/index.html', context)
+
+def edit_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+
+    if request.method == 'POST':
+        task.name = request.POST['name']
+        task.days_worked = request.POST['days_worked']
+        task.estimate_remaining = request.POST['estimate_remaining']
+        task.save()
+        return redirect('schedule:index')    
+    
+    context = {'task': task}
+    return render(request, 'schedule/edit_task.html', context)
