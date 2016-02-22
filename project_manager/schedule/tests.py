4b998dd251134d36841e57b879ea00063cf5eae6
Tim Martin <tim@asymptotic.co.uk>
2016-02-22 17:05:48 +0000

    Added a first unit test

--- project_manager/schedule/tests.py
+++ project_manager/schedule/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+import datetime
+
+from .models import Task, Resource
+
+class TaskTests(TestCase):
+    def test_estimated_end_date(self):
+        worker = Resource()
+
+        first_task = Task(name="first",
+                          orig_estimate=3,
+                          estimate_remaining=3,
+                          resource=worker)
+
+        # Start on a Monday
+        monday = datetime.date(2016, 2, 22)
+
+        self.assertEqual(first_task.estimated_end_date(monday),
+                         datetime.date(2016, 2, 24))
